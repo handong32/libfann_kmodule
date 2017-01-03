7bab1d56f5bc64405b52408cc5a12a3d9156accd
Han <handong@bu.edu>
2017-01-03 12:52:46 -0500

    updates

--- hzjiffies.c
+++ hzjiffies.c
@@ -4,12 +4,27 @@
 #include <linux/kernel.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <linux/jiffies.h>
 
 static struct proc_dir_entry *root_irq_dir;
 static char Buf[PAGE_SIZE];      /* The buffer to store last message */
 static char *Buf_Ptr;           /* A pointer to the buffer */
 static int Buf_Char = 0;       /* The number of characters stored */
 static int Bytes_Read = 0;      /* Number of bytes read since open */
+
+static int
+jiffies_proc_show(struct seq_file *m, void *v)
+{
+    seq_printf(m, "%llu\n",
+	       (unsigned long long) get_jiffies_64());
+    return 0;
+}
+
+static int
+jiffies_proc_open(struct inode *inode, struct file *file)
+{
+    return single_open(file, jiffies_proc_show, NULL);
+}
 
 static int
 hz_show(struct seq_file *m, void *v)
@@ -32,6 +47,14 @@ static const struct file_operations hz_fops = {
     .release    = single_release,
 };
 
+static const struct file_operations jiffies_proc_fops = {
+    .owner      = THIS_MODULE,
+    .open       = jiffies_proc_open,
+    .read       = seq_read,
+    .llseek     = seq_lseek,
+    .release    = single_release,
+};
+
 static int __init
 nn_proc_init(void)
 {
@@ -41,8 +64,11 @@ nn_proc_init(void)
     if (!root_irq_dir)
 	return -1;
 
+    proc_create("jiffies", 0, root_irq_dir, &jiffies_proc_fops);
     proc_create("hz", 0, root_irq_dir, &hz_fops);
-    
+
+    //printk("Created nn module\n");
+
     return 0;
 }
 
